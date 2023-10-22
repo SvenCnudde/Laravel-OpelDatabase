b829f7943b7fe6c824d48047037e7f50b26c62a3
Sven Cnudde <sven.cnudde@hotmail.com>
2023-10-22 19:51:30 +0200

    problemer med git

--- resources/views/console/dashboard.blade.php
+++ resources/views/console/dashboard.blade.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/topnav/main.css?v=1.0.28">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3schools32.css?v=1.0.1">
     <title>Document</title>
 </head>
 <body>
-    <h1>hello from Adam Opel</h1>
+
+    <h1>Hello from Adam Opel</h1>
 </body>
 </html>
